a25579d80951ce075440353abb53121505d18672
paivadul <dul.paiva.comunicaciones@gmail.com>
2024-03-03 22:56:08 -0300

    Creando un componente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import MyNewComponent from './components/MyNewComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a Maraton</li>
+        <li>Feed the dogs</li>
+      </ul>
+      
+      <MyNewComponent />
     </div>
   );
 }
---
+++ src/components/MyFirtsComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyNewComponent extends Component{
+    render(){
+        return(
+            <div>
+                Wer are in MyNewComponent
+            </div>
+        );
+    }
+}
+
+export default MyNewComponent;
